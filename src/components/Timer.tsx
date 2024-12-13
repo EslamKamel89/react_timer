6e7a1d8e2c024fd9063c24a5cb7c809b82f7daa6
EslamKamel89 <elsamkamelforex@gmail.com>
2024-12-13 21:06:54 +0200

    using context to handle stoping all the timers and starting them

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,22 +1,32 @@
 import { useEffect, useRef, useState } from "react";
-import { Timer as TimerProps } from "../store/timer-context.tsx";
+import {
+  Timer as TimerProps,
+  useTimersContext,
+} from "../store/timer-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
   const interval = useRef<number>();
+  const timerCtx = useTimersContext();
+
   if (remainingTime <= 0) {
     clearInterval(interval.current);
   }
   useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prev) => prev - 50);
-    }, 50);
-    interval.current = timer;
+    let timer: number | undefined;
+    if (timerCtx.isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prev) => prev - 50);
+      }, 50);
+      interval.current = timer;
+    } else {
+      if (timer) clearInterval(timer);
+    }
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [timerCtx.isRunning]);
   const formatedRemainingTime = (remainingTime / 1000).toFixed(2);
   return (
     <Container as="article">
